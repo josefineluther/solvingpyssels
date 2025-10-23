d4b56076040c0a20dbaa893cf1e3145a8988247b
josefineluther <josefineluther@hotmail.se>
2025-10-23 16:32:22 +0200

    added fieldset

--- app/createproject/page.tsx
+++ app/createproject/page.tsx
@@ -1,8 +1,66 @@
+import {
+    Field,
+    FieldDescription,
+    FieldLabel,
+    FieldLegend,
+    FieldSet,
+} from '@/components/ui/field'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { Button } from '@/components/ui/button'
+
 export default function CreateProject() {
     return (
-        <div>
-            <h1>Create Project</h1>
-            <p>This is the create project page.</p>
-        </div>
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-center">
+            <FieldSet>
+                <FieldLegend>Create Project</FieldLegend>
+                <FieldDescription>
+                    Fill in the details to create a new project
+                </FieldDescription>
+                <Field>
+                    <FieldLabel htmlFor="name">Project Name</FieldLabel>
+                    <Input
+                        id="name"
+                        autoComplete="off"
+                        placeholder="Knitted Sweater"
+                    />
+                </Field>
+                <Field>
+                    <FieldLabel htmlFor="description">Description</FieldLabel>
+                    <Textarea id="description" />
+                </Field>
+                <Field>
+                    <FieldLabel htmlFor="image">Upload Image</FieldLabel>
+                    <Input id="image" type="file" />
+                </Field>
+                <Field>
+                    <FieldLabel htmlFor="category">Category</FieldLabel>
+                    <Select>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Choose a category" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="crocheting">
+                                Crocheting
+                            </SelectItem>
+                            <SelectItem value="knitting">Knitting</SelectItem>
+                            <SelectItem value="sewing">Sewing</SelectItem>
+                            <SelectItem value="art">Art</SelectItem>
+                            <SelectItem value="craft">Craft</SelectItem>
+                            <SelectItem value="jewelry">Jewelry</SelectItem>
+                            <SelectItem value="other">Other</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </Field>
+                <Button type="submit">Create Project</Button>
+            </FieldSet>
+        </main>
     )
 }
